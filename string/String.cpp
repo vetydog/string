0b7aeb8041d3b927fd8a9ec2326423feff741a87
Pavlik <samvelnagapetian@gmail.com>
2026-04-30 19:41:08 +0200

    fixed

--- string/String.cpp
+++ string/String.cpp
@@ -44,3 +44,35 @@ void String::show(){
 int String::countString() {
     return count;
 }
+
+String operator*(const String& s1, const String& s2) {
+    char result[256] = "";
+    int y = 0;
+
+    for (int i = 0; s1.s[i] != '\0'; i++) {
+
+        bool found = false;
+        for (int j = 0; s2.s[j] != '\0'; j++) {
+            if (s1.s[i] == s2.s[j]) {
+                found = true;
+                break;
+            }
+        }
+
+        bool already = false;
+        for (int x = 0; x < y; x++) {
+            if (result[x] == s1.s[i]) {
+                already = true;
+                break;
+            }
+        }
+
+        if (found && !already) {
+            result[y++] = s1.s[i];
+        }
+    }
+
+    result[y] = '\0';
+
+    return String(result);
+}
--- string/String.h
+++ string/String.h
@@ -20,36 +20,6 @@ public:
     void show();
     static int countString();
 
-    friend String operator*(const String& s1, const String& s2) {
-        char result[256] = "";  
-        int y = 0;
-
-        for (int i = 0; s1.s[i] != '\0'; i++) {
-
-            bool found = false;
-            for (int j = 0; s2.s[j] != '\0'; j++) {
-                if (s1.s[i] == s2.s[j]) {
-                    found = true;
-                    break;
-                }
-            }
-
-            bool already = false;
-            for (int x = 0; x < y; x++) {
-                if (result[x] == s1.s[i]) {
-                    already = true;
-                    break;
-                }
-            }
-
-            if (found && !already) {
-                result[y++] = s1.s[i];
-            }
-        }
-
-        result[y] = '\0';
-
-        return String(result);
-    }
+    friend String operator*(const String& s1, const String& s2);
 };
 
